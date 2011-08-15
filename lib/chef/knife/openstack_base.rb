a5c1a9d59fef5be2ae6db771a9025f8e34411158
Seth Chisamore <schisamo@opscode.com>
2011-08-15 12:56:49 -0400

    [KNIFE_OPENSTACK-2] ensure value is string before empty? check

--- lib/chef/knife/openstack_base.rb
+++ lib/chef/knife/openstack_base.rb
@@ -77,7 +77,8 @@ class Chef
       end
 
       def msg_pair(label, value, color=:cyan)
-        if value && !value.empty?
+        if value
+          if value !value.to_s.empty?
           puts "#{ui.color(label, color)}: #{value}"
         end
       end
