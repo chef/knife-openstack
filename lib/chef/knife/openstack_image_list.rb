aaa9810b12f46f2dd0b505887ac44a22c487c373
mattray <matthewhray@gmail.com>
2012-02-14 10:50:47 -0600

    kernel, architecture, root store and location are not currently supported

--- lib/chef/knife/openstack_image_list.rb
+++ lib/chef/knife/openstack_image_list.rb
@@ -1,6 +1,7 @@
 #
 # Author:: Seth Chisamore (<schisamo@opscode.com>)
-# Copyright:: Copyright (c) 2011 Opscode, Inc.
+# Author:: Matt Ray (<matt@opscode.com>)
+# Copyright:: Copyright (c) 2011-2012 Opscode, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -32,29 +33,29 @@ class Chef
 
         image_list = [
           ui.color('ID', :bold),
-          ui.color('Kernel ID', :bold),
-          ui.color('Architecture', :bold),
-          ui.color('Root Store', :bold),
           ui.color('Name', :bold),
-          ui.color('Location', :bold)
+          # ui.color('Kernel ID', :bold),
+          # ui.color('Architecture', :bold),
+          # ui.color('Root Store', :bold),
+          # ui.color('Location', :bold)
         ]
 
         connection.images.sort_by do |image|
-          [image.name, image.id].compact
+          [image.name.downcase, image.id].compact
         end.each do |image|
           image_list << image.id
-          image_list << image.kernel_id
-          image_list << image.architecture
-          image_list << image.root_device_type
           image_list << image.name
-          image_list << image.location
+          # image_list << image.kernel_id
+          # image_list << image.architecture
+          # image_list << image.root_device_type
+          # image_list << image.location
         end
 
         image_list = image_list.map do |item|
           item.to_s
         end
 
-        puts ui.list(image_list, :columns_across, 6)
+        puts ui.list(image_list, :columns_across, 2)
       end
     end
   end
