ee8f88456f84b5f1992ec82d85ef5aac9959cf16
mattray <matthewhray@gmail.com>
2012-02-15 15:47:01 -0600

    now works with osapi and warns on ids that aren't found

--- lib/chef/knife/openstack_server_delete.rb
+++ lib/chef/knife/openstack_server_delete.rb
@@ -1,6 +1,7 @@
 #
 # Author:: Seth Chisamore (<schisamo@opscode.com>)
-# Copyright:: Copyright (c) 2011 Opscode, Inc.
+# Author:: Matt Ray (<matt@opscode.com>)
+# Copyright:: Copyright (c) 2011-2012 Opscode, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -31,29 +32,28 @@ class Chef
         validate!
 
         @name_args.each do |instance_id|
-          server = connection.servers.get(instance_id)
+          begin
+            server = connection.servers.get(instance_id)
 
-          msg_pair("Instance ID", server.id)
-          msg_pair("Flavor", server.flavor_id)
-          msg_pair("Image", server.image_id)
-          msg_pair("Availability Zone", server.availability_zone)
-          msg_pair("Security Groups", server.groups.join(", "))
-          msg_pair("SSH Key", server.key_name)
-          msg_pair("Public DNS Name", server.dns_name)
-          msg_pair("Public IP Address", server.public_ip_address)
-          msg_pair("Private DNS Name", server.private_dns_name)
-          msg_pair("Private IP Address", server.private_ip_address)
+            msg_pair("Instance ID", server.id)
+            msg_pair("Instance Name", server.name)
+            msg_pair("Flavor", server.flavor['id'])
+            msg_pair("Image", server.image['id'])
+            msg_pair("Public IP Address", server.public_ip_address['addr'])
+            msg_pair("Private IP Address", server.private_ip_address['addr'])
 
-          puts "\n"
-          confirm("Do you really want to delete this server")
+            puts "\n"
+            confirm("Do you really want to delete this server")
 
-          server.destroy
+            server.destroy
 
-          ui.warn("Deleted server #{server.id}")
+            ui.warn("Deleted server #{server.id}")
+          rescue NoMethodError
+            ui.error("Could not locate server '#{instance_id}'.")
+          end
         end
       end
 
     end
   end
 end
-
