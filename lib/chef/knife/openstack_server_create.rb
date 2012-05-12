df7c34158b0a164ffa432b3731a0ce298cb6ff10
mattray <matthewhray@gmail.com>
2012-05-12 00:48:01 -0500

    chef-full is now default bootstrap template

--- lib/chef/knife/openstack_server_create.rb
+++ lib/chef/knife/openstack_server_create.rb
@@ -95,7 +95,7 @@ class Chef
       option :distro,
       :short => "-d DISTRO",
       :long => "--distro DISTRO",
-      :description => "Bootstrap a distro using a template (default is chef-full)",
+      :description => "Bootstrap a distro using a template; default is 'chef-full'",
       :proc => Proc.new { |d| Chef::Config[:knife][:distro] = d },
       :default => "chef-full"
 
