d6577740511fa54d048b8e6fff0476c423de1191
mattray <matthewhray@gmail.com>
2012-06-03 08:50:23 -0500

    initial purge support, doesn't work since node names don't match ID

--- lib/chef/knife/openstack_server_delete.rb
+++ lib/chef/knife/openstack_server_delete.rb
@@ -19,6 +19,10 @@
 
 require 'chef/knife/openstack_base'
 
+# These two are needed for the '--purge' deletion case
+require 'chef/node'
+require 'chef/api_client'
+
 class Chef
   class Knife
     class OpenstackServerDelete < Knife
@@ -26,6 +30,33 @@ class Chef
       include Knife::OpenstackBase
 
       banner "knife openstack server delete SERVER [SERVER] (options)"
+
+      option :purge,
+        :short => "-P",
+        :long => "--purge",
+        :boolean => true,
+        :default => false,
+        :description => "Destroy corresponding node and client on the Chef Server, in addition to destroying the OpenStack node itself. Assumes node and client have the same name as the server (if not, add the '--node-name' option)."
+
+      option :chef_node_name,
+        :short => "-N NAME",
+        :long => "--node-name NAME",
+        :description => "The name of the node and client to delete, if it differs from the server name. Only has meaning when used with the '--purge' option."
+
+      # Extracted from Chef::Knife.delete_object, because it has a
+      # confirmation step built in... By specifying the '--purge'
+      # flag (and also explicitly confirming the server destruction!)
+      # the user is already making their intent known.  It is not
+      # necessary to make them confirm two more times.
+      def destroy_item(klass, name, type_name)
+        begin
+          object = klass.load(name)
+          object.destroy
+          ui.warn("Deleted #{type_name} #{name}")
+        rescue Net::HTTPServerException
+          ui.warn("Could not find a #{type_name} named #{name} to delete!")
+        end
+      end
 
       def run
 
@@ -39,6 +70,9 @@ class Chef
             msg_pair("Instance Name", server.name)
             msg_pair("Flavor", server.flavor['id'])
             msg_pair("Image", server.image['id'])
+            # msg_pair("Availability Zone", server.availability_zone)
+            # msg_pair("Security Groups", server.groups.join(", "))
+            # msg_pair("SSH Key", server.key_name)
             msg_pair("Public IP Address", server.public_ip_address['addr'])
             msg_pair("Private IP Address", server.private_ip_address['addr'])
 
@@ -48,6 +82,15 @@ class Chef
             server.destroy
 
             ui.warn("Deleted server #{server.id}")
+
+            if config[:purge]
+              thing_to_delete = config[:chef_node_name] || instance_id
+              destroy_item(Chef::Node, thing_to_delete, "node")
+              destroy_item(Chef::ApiClient, thing_to_delete, "client")
+            else
+              ui.warn("Corresponding node and client for the #{instance_id} server were not deleted and remain registered with the Chef Server")
+            end
+
           rescue NoMethodError
             ui.error("Could not locate server '#{instance_id}'.")
           end
