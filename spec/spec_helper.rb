7cad60314b30d422640431554811eadc7d7c6b51
Ameya Varade <ameya.varade@clogeny.com>
2013-05-29 12:08:20 +0530

    Creating a dummy knife.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,7 @@
 $:.unshift File.expand_path('../../lib', __FILE__)
-require 'chef/knife/bootstrap'
-require 'chef/knife/winrm_base'
 require 'chef/knife/openstack_server_create'
 require "securerandom"
 require 'tmpdir'
-require 'rbconfig'
 require 'fileutils'
 require File.expand_path(File.dirname(__FILE__) +'/utils/knifeutils')
 require File.expand_path(File.dirname(__FILE__) +'/utils/matchers')
@@ -31,37 +28,10 @@ def match_status(test_run_expect)
   else
     should have_outcome :status => 0
   end
-end 
-
-def is_windows?
-  if(ENV['OS'] != nil)
-    ENV['OS'].downcase.include?("windows")
-  end
-end
-
-def is_linux?
-  RbConfig::CONFIG["arch"].include?("linux")
-end
-
-def create_dummy_validation_pem() 
-  data_to_write = "../integration/config/validation.pem"
-  if(is_windows?)
-    if(!File.exist?('C:/chef/validation.pem'))
-      FileUtils.mkpath 'C:/chef'
-      create_file("C:/chef", "validation.pem", data_to_write)
-    end
-  end
-  if(is_linux?)
-    if(!File.exist?('/etc/chef/validation.pem'))
-      # FIXME a dummy file needs to be present at '/etc/chef'
-      FileUtils.mkpath '/etc/chef'
-      create_file("/etc/chef", "validation.pem", data_to_write)
-    end
-  end
 end
 
 def create_file(file_dir, file_name, data_to_write_file_path)
   data_to_write = File.read(File.expand_path("#{data_to_write_file_path}", __FILE__))
   File.open("#{file_dir}/#{file_name}", 'w') {|f| f.write(data_to_write)}
-  puts "Creating: #{file_dir}/#{file_name}" 
+  puts "Creating: #{file_name}"
 end
--- spec/utils/knifeutils.rb
+++ spec/utils/knifeutils.rb
@@ -1,29 +1,21 @@
-
 require 'active_support/concern'
 require 'mixlib/shellout'
 module RSpec
   module KnifeUtils    
     extend ActiveSupport::Concern
-    included do      
+    included do
       subject { knife_run }
       let(:knife_run) { run command }
-      let(:command)   { fail 'Define let(:command) in the spec' }      
-      let(:grep_cmd) { "" }
+      let(:command)   { fail 'Define let(:command) in the spec' }
       let(:cmd_stdout) { @op }
-
-      # Convenience method for creating a Mixlib::ShellOut representation
-      # of a knife command in our test repository
-      def shell_out_command(cmd, msg)
-        shell_out = Mixlib::ShellOut.new("#{cmd}" + " -V")        
-        @op = shell_out.tap(&:run_command).stdout        
-        return shell_out
-      end      
 
       # Convenience method for actually running a knife command in our
       # testing repository.  Returns the Mixlib::Shellout object ready for
       # inspection.
       def run(command_line)
-        shell_out_command(command_line, "Initiating knife command...")
+        shell_out = Mixlib::ShellOut.new("#{command_line}" + " -V")
+        @op = shell_out.tap(&:run_command).stdout        
+        return shell_out
       end
 
       def knife(knife_command)
