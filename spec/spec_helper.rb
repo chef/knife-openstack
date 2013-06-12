fcc9045a8b34471414f289e38a0524158b2bcea6
Ameya Varade <ameya.varade@clogeny.com>
2013-06-12 12:05:07 +0530

    check if configuration file is present.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,3 +35,15 @@ def create_file(file_dir, file_name, data_to_write_file_path)
   File.open("#{file_dir}/#{file_name}", 'w') {|f| f.write(data_to_write)}
   puts "Creating: #{file_name}"
 end
+
+def is_config_present 
+  is_config_present = File.exist?(File.expand_path("../integration/config/environment.yml", __FILE__)) 
+  if(!is_config_present)
+    puts "\nSkipping the integration tests for knife openstack commands"
+    puts "\nPlease make sure environment.yml is present and set with valid credentials."
+    puts "\nPlease look for a sample file at spec/integration/config/environment.yml.sample"
+    puts "\nPlease make sure openstack.pem is present and set with valid key pair content. This content should match for key pair name mentioned in environment.yml at attribute 'key_pair: key_pair_name'"
+    puts "\nBy default openstack.pem contains dummy key pair content.\n"
+  end
+  is_config_present
+end
