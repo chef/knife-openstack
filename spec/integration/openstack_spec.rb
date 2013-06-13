6f875e7f4cee873cf8db253d226ba04d96093b6b
Ameya Varade <ameya.varade@clogeny.com>
2013-06-13 16:17:55 +0530

    code re-factor and additional test cases

--- spec/integration/openstack_spec.rb
+++ spec/integration/openstack_spec.rb
@@ -17,6 +17,10 @@
 
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+def get_gem_file_name
+  "knife-openstack-" + Knife::OpenStack::VERSION + ".gem"
+end
+
 def append_openstack_creds(is_list_cmd = false)
   openstack_config = YAML.load(File.read(File.expand_path("../config/environment.yml", __FILE__)))
   openstack_creds_cmd = " --openstack-username #{openstack_config['development']['openstack_username']} --openstack-password #{openstack_config['development']['openstack_password']} --openstack-api-endpoint #{openstack_config['development']['openstack_auth_url']}"
@@ -47,108 +51,165 @@ def create_node_name()
   @name_node  = "ostsp-#{SecureRandom.hex(4)}"
 end
 
-def linux_template_file_path
-  "#{temp_dir}/chef-full-chef-zero.erb"
+def init_openstack_test
+  init_test
+  begin
+    data_to_write = File.read(File.expand_path("../config/openstack.pem", __FILE__))
+    File.open("#{temp_dir}/openstack.pem", 'w') {|f| f.write(data_to_write)}
+  rescue
+    puts "Error while creating file - openstack.pem"
+  end
+  #create_file("#{temp_dir}", "openstack.pem", "../integration/config/openstack.pem")
 end
 
-def windows_template_file_path
-  "#{temp_dir}/windows-chef-client-msi.erb"
-end
-
-def init_test
-  puts "\nCreating Test Data\n"
-  create_file("#{temp_dir}", "validation.pem", "../integration/config/validation.pem" )
-  create_file("#{temp_dir}", "chef-full-chef-zero.erb", "../integration/templates/chef-full-chef-zero.erb" )
-  create_file("#{temp_dir}", "windows-chef-client-msi.erb", "../integration/templates/windows-chef-client-msi.erb" )
-  create_file("#{temp_dir}", "openstack.pem", "../integration/config/openstack.pem")
-  create_file("#{temp_dir}", "knife.rb", "../integration/config/knife.rb")
-end
-
-def cleanup_test_data
-  puts "\nCleaning Test Data\n"
-  FileUtils.rm_rf("#{temp_dir}")
-  puts "\nDone\n"
-end
-
-describe 'knife' do
-  include RSpec::KnifeUtils
-  before(:all) { init_test }
+describe 'knife-openstack' do
+  include KnifeTestBed
+  include RSpec::KnifeTestUtils
+  before(:all) { init_openstack_test }
   after(:all) { cleanup_test_data }
-  context 'openstack integration test - ' do
-    context 'create server' do
-      cmd_out = ""
-      before(:each) { create_node_name }
-      let(:command) { "knife openstack server create -N #{@name_node}"+
-      " -I 9d155c01-1652-43bf-95f3-30893c40d423 -f 2"+
-      " --template-file " + linux_template_file_path +
-      " --server-url http://localhost:8889" +
-      " --yes" +
-      append_openstack_creds() }
-      after(:each)  { cmd_out = "#{cmd_stdout}" }
+  context 'gem' do
+    context 'build' do
+      let(:command) { "gem build knife-openstack.gemspec" }
       it 'should succeed' do
         match_status("should succeed")
       end
+    end
 
-      context "delete server after create" do
-        let(:command) { delete_instance_cmd(cmd_out) }
-        it "should succeed" do
+    context 'install ' do
+      let(:command) { "gem install " + get_gem_file_name  } 
+      it 'should succeed' do
+        match_status("should succeed")
+      end
+    end
+
+    describe 'knife' do
+      context 'openstack' do
+        context 'flavor list --help' do
+         let(:command) { "knife openstack flavor list --help" } 
+           it 'should succeed' do
+            match_stdout(/--help/)
+          end
+        end
+
+        context 'group list --help' do
+         let(:command) { "knife openstack group list --help" } 
+           it 'should succeed' do
+            match_stdout(/--help/)
+          end
+        end
+
+        context 'image list --help' do
+         let(:command) { "knife openstack image list --help" } 
+           it 'should succeed' do
+            match_stdout(/--help/)
+          end
+        end
+
+        context 'server create --help' do
+         let(:command) { "knife openstack server create --help" } 
+           it 'should succeed' do
+            match_stdout(/--help/)
+          end
+        end
+
+        context 'server delete --help' do
+         let(:command) { "knife openstack server delete --help" } 
+           it 'should succeed' do
+            match_stdout(/--help/)
+          end
+        end
+
+        context 'server list --help' do
+         let(:command) { "knife openstack server list --help" } 
+           it 'should succeed' do
+            match_stdout(/--help/)
+          end
+        end
+      end      
+    end
+
+    describe 'knife' do
+      context 'create server' do
+        cmd_out = ""
+        before(:each) { create_node_name }
+        let(:command) { "knife openstack server create -N #{@name_node}"+
+        " -I 9d155c01-1652-43bf-95f3-30893c40d423 -f 2"+
+        " --template-file " + get_linux_template_file_path +
+        " --server-url http://localhost:8889" +
+        " --yes" +
+        append_openstack_creds() }
+        after(:each)  { cmd_out = "#{cmd_stdout}" }
+        it 'should succeed' do
+          match_status("should succeed")
+        end
+
+        context "delete server after create" do
+          let(:command) { delete_instance_cmd(cmd_out) }
+          it "should succeed" do
+            match_status("should succeed")
+          end
+        end
+      end
+
+      context 'create server (for windows)' do
+        cmd_out = ""
+        before(:each) { create_node_name }
+        let(:command) { "knife openstack server create -N #{@name_node}"+
+        " -I 18f6310a-f0b2-41ff-add5-a16e0f79ff6f -f 18"+
+        " --template-file " + get_windows_msi_template_file_path +
+        " --server-url http://localhost:8889" +
+        " --bootstrap-protocol winrm" +
+        " --winrm-user Administrator" +
+        " --winrm-password Chef2UncleNed!" +
+        " --yes" +
+        append_openstack_creds_for_windows() }
+        after(:each)  { cmd_out = "#{cmd_stdout}" }
+        it 'should succeed' do
+          match_status("should succeed")
+        end
+
+        context "delete server after create" do
+          let(:command) { delete_instance_cmd(cmd_out) }
+          it "should succeed" do
+            match_status("should succeed")
+          end
+        end
+      end
+
+      context 'server list' do
+        let(:command) { "knife openstack server list" + append_openstack_creds(is_list_cmd = true) }
+        it 'should succeed' do
+          match_status("should succeed")
+        end
+      end
+
+      context 'flavor list' do
+        let(:command) { "knife openstack flavor list" + append_openstack_creds(is_list_cmd = true) }
+        it 'should succeed' do
+          match_status("should succeed")
+        end
+      end
+
+      context 'image list' do
+        let(:command) { "knife openstack image list" + append_openstack_creds(is_list_cmd = true) }
+        it 'should succeed' do
+          match_status("should succeed")
+        end
+      end
+
+      context 'group  list' do
+        let(:command) { "knife openstack group list" + append_openstack_creds(is_list_cmd = true) }
+        it 'should succeed' do
           match_status("should succeed")
         end
       end
     end
 
-    context 'create server (for windows)' do
-      cmd_out = ""
-      before(:each) { create_node_name }
-      let(:command) { "knife openstack server create -N #{@name_node}"+
-      " -I 18f6310a-f0b2-41ff-add5-a16e0f79ff6f -f 18"+
-      " --template-file " + windows_template_file_path +
-      " --server-url http://localhost:8889" +
-      " --bootstrap-protocol winrm" +
-      " --winrm-user Administrator" +
-      " --winrm-password Chef2UncleNed!" +
-      " --yes" +
-      append_openstack_creds_for_windows() }
-      after(:each)  { cmd_out = "#{cmd_stdout}" }
-      it 'should succeed' do
-        match_status("should succeed")
-      end
-
-      context "delete server after create" do
-        let(:command) { delete_instance_cmd(cmd_out) }
-        it "should succeed" do
-          match_status("should succeed")
-        end
-      end
-    end
-
-    context 'server list' do
-      let(:command) { "knife openstack server list" + append_openstack_creds(is_list_cmd = true) }
+    context 'uninstall ' do
+      let(:command) { "gem uninstall knife-openstack -v '#{Knife::OpenStack::VERSION}'" }
       it 'should succeed' do
         match_status("should succeed")
       end
     end
-
-    context 'flavor list' do
-      let(:command) { "knife openstack flavor list" + append_openstack_creds(is_list_cmd = true) }
-      it 'should succeed' do
-        match_status("should succeed")
-      end
-    end
-
-    context 'image list' do
-      let(:command) { "knife openstack image list" + append_openstack_creds(is_list_cmd = true) }
-      it 'should succeed' do
-        match_status("should succeed")
-      end
-    end
-
-    context 'group  list' do
-      let(:command) { "knife openstack group list" + append_openstack_creds(is_list_cmd = true) }
-      it 'should succeed' do
-        match_status("should succeed")
-      end
-    end
-
   end
 end
