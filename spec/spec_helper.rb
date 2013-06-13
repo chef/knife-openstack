1a8b790fd2521136386d132227f11b1889df9559
Ameya Varade <ameya.varade@clogeny.com>
2013-06-13 16:17:25 +0530

    code refactoring
    This code is moved under knife-cloud

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,8 @@
 $:.unshift File.expand_path('../../lib', __FILE__)
 require 'chef/knife/openstack_server_create'
 require "securerandom"
-require 'tmpdir'
-require 'fileutils'
-require File.expand_path(File.dirname(__FILE__) +'/utils/knifeutils')
-require File.expand_path(File.dirname(__FILE__) +'/utils/matchers')
-
-def temp_dir
-  @_temp_dir ||= Dir.mktmpdir
-end
+require 'knife-openstack/version'
+require 'test/knife-utils/test_bed'
 
 def find_instance_id(instance_name, file)
   file.lines.each do |line|
@@ -16,22 +10,4 @@ def find_instance_id(instance_name, file)
       return "#{line}".split(': ')[1].strip
     end
   end
-end
-
-def match_status(test_run_expect)
-  if "#{test_run_expect}" == "should fail"
-    should_not have_outcome :status => 0
-  elsif "#{test_run_expect}" == "should succeed"
-    should have_outcome :status => 0
-  elsif "#{test_run_expect}" == "should return empty list"
-    should have_outcome :status => 0
-  else
-    should have_outcome :status => 0
-  end
-end
-
-def create_file(file_dir, file_name, data_to_write_file_path)
-  data_to_write = File.read(File.expand_path("#{data_to_write_file_path}", __FILE__))
-  File.open("#{file_dir}/#{file_name}", 'w') {|f| f.write(data_to_write)}
-  puts "Creating: #{file_name}"
 end
