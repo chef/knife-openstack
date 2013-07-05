b6a0390d36acc8952a0bd242c5c611fd28d1f824
kaustubh-d <kaustubh@clogeny.com>
2013-07-05 13:32:49 +0530

    common helpers used across commands.

--- lib/chef/knife/openstack_helpers.rb
+++ lib/chef/knife/openstack_helpers.rb
@@ -2,27 +2,44 @@ require 'chef/knife/cloud/openstack_service_options'
 
 class Chef
   class Knife
-    module OpenstackHelpers
+    class Cloud
+      module OpenstackHelpers
 
-      def primary_private_ip_address(addresses)
-          return addresses['private'].last['addr'] if addresses['private']
+        def primary_private_ip_address(addresses)
+            return addresses['private'].last['addr'] if addresses['private']
+        end
+
+        #we use last since the floating IP goes there
+        def primary_public_ip_address(addresses)
+            return addresses['public'].last['addr'] if addresses['public']
+        end
+
+        def is_image_windows?
+          # Openstack image info does not have os type, so we use image_os_type cli option to interpret if we are creating windows server.
+          os_type = locate_config_value(:image_os)
+          os_type.nil? ? false : (os_type.downcase == 'windows')
+        end
+
+        def create_service_instance
+          OpenstackService.new
+        end
+
+        def validate!(keys=[:openstack_username, :openstack_password, :openstack_auth_url])
+          errors = []
+
+          keys.each do |k|
+            pretty_key = k.to_s.gsub(/_/, ' ').gsub(/\w+/){ |w| (w =~ /(ssh)|(aws)/i) ? w.upcase  : w.capitalize }
+            if Chef::Config[:knife][k].nil?
+              errors << "You did not provided a valid '#{pretty_key}' value."
+            end
+          end
+
+          if errors.each{|e| ui.error(e)}.any?
+            exit 1
+          end
+        end
+
       end
-
-      #we use last since the floating IP goes there
-      def primary_public_ip_address(addresses)
-          return addresses['public'].last['addr'] if addresses['public']
-      end
-
-      def is_image_windows?
-        # Openstack image info does not have os type, so we use windows_bootstrap_protocol to interpret if we are creating windows server.
-        !config[:windows_bootstrap_protocol].nil? ? true : false
-      end
-
-      def locate_config_value(key)
-        key = key.to_sym
-        Chef::Config[:knife][key] || config[key]
-      end
-
     end
   end
 end
