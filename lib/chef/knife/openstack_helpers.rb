16e69091b45d3c27c92c24254938ff4f882b4134
Mukta A <mukta.aphale@clogeny.com>
2013-08-14 16:02:37 +0530

    Fixed bug for server list when server in error state

--- lib/chef/knife/openstack_helpers.rb
+++ lib/chef/knife/openstack_helpers.rb
@@ -6,12 +6,12 @@ class Chef
       module OpenstackHelpers
 
         def primary_private_ip_address(addresses)
-            return addresses['private'].last['addr'] if addresses['private']
+          return addresses['private'].last['addr'] if addresses['private'] && !addresses['private'].empty?
         end
 
         #we use last since the floating IP goes there
         def primary_public_ip_address(addresses)
-            return addresses['public'].last['addr'] if addresses['public']
+          return addresses['public'].last['addr'] if addresses['public'] && !addresses['public'].empty?
         end
 
         def is_image_windows?
--- spec/functional/server_list_func_spec.rb
+++ spec/functional/server_list_func_spec.rb
@@ -8,7 +8,8 @@ describe Chef::Knife::Cloud::OpenstackServerList do
   context "functionality" do
     before do
       resources = [ TestResource.new({:id => "resource-1", :name => "ubuntu01", :addresses => {"public"=>[{"version"=>4, "addr"=>"172.31.6.132"}], "private"=>[{"version"=>4, "addr"=>"172.31.6.133"}]}, :flavor => {"id" => "1"}, :image => {"id" => "image1"}, :key_name => "keypair", :state => "ACTIVE"}),
-                     TestResource.new({:id => "resource-2", :name => "windows2008", :addresses => {"public"=>[{"version"=>4, "addr"=>"172.31.6.132"}]}, :flavor => {"id" => "id2"}, :image => {"id" => "image2"}, :key_name => "keypair", :state => "ACTIVE"})
+                     TestResource.new({:id => "resource-2", :name => "windows2008", :addresses => {"public"=>[{"version"=>4, "addr"=>"172.31.6.132"}]}, :flavor => {"id" => "id2"}, :image => {"id" => "image2"}, :key_name => "keypair", :state => "ACTIVE"}),
+                     TestResource.new({:id => "resource-3-err", :name => "windows2008", :addresses => {"public"=>[], "private"=>[]}, :flavor => {"id" => "id2"}, :image => {"id" => "image2"}, :key_name => "keypair", :state => "ERROR"})
                    ]
       instance.stub(:query_resource).and_return(resources)
       instance.stub(:puts)
@@ -19,7 +20,8 @@ describe Chef::Knife::Cloud::OpenstackServerList do
     it "lists formatted list of resources" do
       instance.ui.should_receive(:list).with(["Instance ID", "Name", "Public IP", "Private IP", "Flavor", "Image", "Keypair", "State",
                                               "resource-1", "ubuntu01", "172.31.6.132", "172.31.6.133", "1", "image1","keypair", "ACTIVE",
-                                              "resource-2", "windows2008", "172.31.6.132", nil, "id2", "image2", "keypair", "ACTIVE"], :uneven_columns_across, 8)
+                                              "resource-2", "windows2008", "172.31.6.132", nil, "id2", "image2", "keypair", "ACTIVE",
+                                              "resource-3-err", "windows2008", nil, nil, "id2", "image2", "keypair", "ERROR"], :uneven_columns_across, 8)
       instance.run
     end
   end
