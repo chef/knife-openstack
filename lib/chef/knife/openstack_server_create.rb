2548201ed6e4d0a82e1304654636d04d55dff3a5
Mukta A <mukta.aphale@clogeny.com>
2013-08-19 13:05:26 +0530

    Auto generation of chef_node_name is handled in knife-cloud now

--- lib/chef/knife/openstack_server_create.rb
+++ lib/chef/knife/openstack_server_create.rb
@@ -39,8 +39,8 @@ class Chef
             # setup the create options
             @create_options = {
               :server_def => {
-                #servers require a name, generate one if not passed
-                :name => get_node_name(locate_config_value(:chef_node_name)),
+                #servers require a name, knife-cloud generates the chef_node_name
+                :name => config[:chef_node_name],
                 :image_ref => locate_config_value(:image),
                 :flavor_ref => locate_config_value(:flavor),
                 :security_groups => locate_config_value(:openstack_security_groups),
