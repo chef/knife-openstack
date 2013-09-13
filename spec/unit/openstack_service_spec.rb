9db9f09c7835f3bf69cbd894d4051b8513872390
siddheshwar-more <siddheshwar.more@clogeny.com>
2013-09-13 16:36:25 +0530

    Added changes for --api-endpoint option

--- spec/unit/openstack_service_spec.rb
+++ spec/unit/openstack_service_spec.rb
@@ -1,8 +1,30 @@
 require 'spec_helper'
 require 'chef/knife/cloud/openstack_service'
-require 'chef/knife/cloud/fog/service'
-require 'support/shared_examples_for_fog_service'
 
 describe Chef::Knife::Cloud::OpenstackService do
-  it_behaves_like Chef::Knife::Cloud::FogService, Chef::Knife::Cloud::OpenstackService.new
+  describe "#add_api_endpoint" do
+    before(:each) do
+      @api_endpoint = "https://test_openstack_api_endpoint"
+      Chef::Config[:knife][:api_endpoint] = @api_endpoint
+      @instance = Chef::Knife::Cloud::OpenstackService.new
+    end
+
+    after(:each) do
+      Chef::Config[:knife].delete(:api_endpoint)
+    end
+
+    it "sets the api_endpoint in auth params" do
+      @instance.instance_variable_get(:@auth_params)[:openstack_auth_url].should == nil
+      @instance.add_api_endpoint
+      @instance.instance_variable_get(:@auth_params)[:openstack_auth_url].should == @api_endpoint
+    end
+
+    it "does not set the endpoint when --api-endpoint option is missing" do
+      Chef::Config[:knife][:api_endpoint] = nil
+      @instance.instance_variable_get(:@auth_params)[:openstack_auth_url].should == nil
+      @instance.add_api_endpoint
+      @instance.instance_variable_get(:@auth_params)[:openstack_auth_url].should_not == @api_endpoint
+      @instance.instance_variable_get(:@auth_params)[:openstack_auth_url].should == nil
+    end
+  end
 end
