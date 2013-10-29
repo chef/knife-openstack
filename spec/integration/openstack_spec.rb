32c270381a2192934f61351803b06d3cd632994e
siddheshwar-more <siddheshwar.more@clogeny.com>
2013-10-29 21:16:12 +0530

    Umarked pending tests

--- spec/integration/openstack_spec.rb
+++ spec/integration/openstack_spec.rb
@@ -67,7 +67,6 @@ describe 'knife-openstack' , :if => is_config_present do
     context 'build' do
       let(:command) { "gem build knife-openstack.gemspec" }
       it 'should successfully build the knife-openstack gem using knife-openstack.gemspec.' do
-        pending "for test"
         match_status("should succeed")
       end
     end
@@ -75,7 +74,6 @@ describe 'knife-openstack' , :if => is_config_present do
     context 'install ' do
       let(:command) { "gem install " + get_gem_file_name  }
       it 'should successfully install the gem on the target system.' do
-        pending "for test"
         match_status("should succeed")
       end
     end
