20d3cc51d4fe1e0ee571138ae27aceae3bb8456a
siddheshwar-more <siddheshwar.more@clogeny.com>
2014-05-22 16:58:15 +0530

    Added changes to fix fog deprecated warning for group list

--- spec/unit/openstack_group_list_spec.rb
+++ spec/unit/openstack_group_list_spec.rb
@@ -39,5 +39,14 @@ describe Chef::Knife::Cloud::OpenstackGroupList do
     end
   end
 
+  context "#list" do
+    before(:each) do
+      @security_groups = [TestResource.new({ "name" => "Unrestricted","description" => "testdescription", "security_group_rules" => [TestResource.new({"from_port"=>636, "group"=>{}, "ip_protocol"=>"tcp", "to_port"=>636, "parent_group_id"=>14, "ip_range"=>{"cidr"=>"0.0.0.0/0"}, "id"=>183})]})]
+    end
 
+    it "returns group list" do
+      instance.ui.should_receive(:list).with(["Name", "Protocol", "From", "To", "CIDR", "Description", "Unrestricted", "tcp", "636", "636", "0.0.0.0/0", "testdescription"],:uneven_columns_across, 6)
+      instance.list(@security_groups)
+    end
+  end
 end
