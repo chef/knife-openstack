58c3199362daa6605a63c9a1f4e4b2966ad4dd02
Prabhu Das <prabhu.das@clogeny.com>
2014-05-30 18:54:06 +0530

    Fixed broken test for group list.

--- spec/functional/group_list_func_spec.rb
+++ spec/functional/group_list_func_spec.rb
@@ -7,9 +7,31 @@ describe Chef::Knife::Cloud::OpenstackGroupList do
 
   context "functionality" do
     before do
-      resources = [ TestResource.new({:name => "Unrestricted", :rules => ["from_port"=>1, "group"=>{}, "ip_protocol"=>"tcp", "to_port"=>65535, "parent_group_id"=>14, "ip_range"=>{"cidr"=>"0.0.0.0/0"}, "id"=>88], :description => "All ports open"}),
-                     TestResource.new({:name => "WindowsDomain", :rules => ["from_port"=>22, "group"=>{}, "ip_protocol"=>"tcp", "to_port"=>22, "parent_group_id"=>12, "ip_range"=>{"cidr"=>"0.0.0.0/0"}, "id"=>58], :description => "Allows common protocols useful in a Windows domain"})
-                   ]
+      resources = [ TestResource.new({ "name" => "Unrestricted",
+                                      "description" => "All ports open",
+                                      "security_group_rules" => [TestResource.new({"from_port" => 1,
+                                                                                   "group" => {},
+                                                                                   "ip_protocol" => "tcp",
+                                                                                   "to_port" => 636,
+                                                                                   "parent_group_id" => 14,
+                                                                                   "ip_range" => {"cidr" => "0.0.0.0/0"},
+                                                                                   "id" => 1
+                                                                                   })
+                                                                ]
+                                    }),
+                    TestResource.new({ "name" => "WindowsDomain",
+                                      "description" => "Allows common protocols useful in a Windows domain",
+                                      "security_group_rules" => [TestResource.new({"from_port" => 22,
+                                                                                   "group" => {},
+                                                                                   "ip_protocol" => "tcp",
+                                                                                   "to_port" => 636,
+                                                                                   "parent_group_id" => 14,
+                                                                                   "ip_range" => {"cidr" => "0.0.0.0/0"},
+                                                                                   "id" => 2
+                                                                                   })
+                                                                ]
+                                    })
+                  ]
       instance.stub(:query_resource).and_return(resources)
       instance.stub(:puts)
       instance.stub(:create_service_instance).and_return(Chef::Knife::Cloud::Service.new)
@@ -18,8 +40,8 @@ describe Chef::Knife::Cloud::OpenstackGroupList do
 
     it "lists formatted list of resources" do
       instance.ui.should_receive(:list).with(["Name", "Protocol", "From", "To", "CIDR", "Description",
-                                              "Unrestricted", "tcp", "1", "65535", "0.0.0.0/0", "All ports open",
-                                              "WindowsDomain", "tcp", "22", "22", "0.0.0.0/0", "Allows common protocols useful in a Windows domain"], :uneven_columns_across, 6)
+                                              "Unrestricted", "tcp", "1", "636", "0.0.0.0/0", "All ports open",
+                                              "WindowsDomain", "tcp", "22", "636", "0.0.0.0/0", "Allows common protocols useful in a Windows domain"], :uneven_columns_across, 6)
       instance.run
     end
   end
