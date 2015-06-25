a508e69a205cfa5584e6e82bead8a6cf02f9a2fc
Vasu1105 <vasundhara.jagdale@clogeny.com>
2015-06-25 15:32:11 +0530

    Added command to list floating ips

---
+++ lib/chef/knife/openstack_floating_ip_list.rb
@@ -0,0 +1,33 @@
+# Author:: Prabhu Das (<prabhu.das@clogeny.com>)
+# Copyright:: Copyright (c) 2014 Chef Software, Inc.
+
+require 'chef/knife/cloud/list_resource_command'
+require 'chef/knife/openstack_helpers'
+require 'chef/knife/cloud/openstack_service_options'
+
+class Chef
+  class Knife
+    class Cloud
+      class OpenstackFloatingIpList < ResourceListCommand
+        include OpenstackHelpers
+        include OpenstackServiceOptions
+
+        banner 'knife openstack floaing ip list (options)'
+
+        def before_exec_command
+          # set columns_with_info map
+          @columns_with_info = [
+            { label: 'Instance ID', key: 'instance_id' },
+            { label: 'IP Address', key: 'ip' },
+            { label: 'Fixed IP', key: 'fixed_ip' },
+            { label: 'Floating IP Pool', key: 'pool' }
+          ]
+        end
+
+        def query_resource
+          @service.list_floating_ips
+        end
+      end
+    end
+  end
+end
