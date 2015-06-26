516aed88daad72edfbec6f6fd4f029570857a3cb
Vasu1105 <vasundhara.jagdale@clogeny.com>
2015-06-26 16:34:02 +0530

    updated specs

--- spec/functional/floating_ip_list_func_spec.rb
+++ spec/functional/floating_ip_list_func_spec.rb
@@ -25,21 +25,22 @@ describe Chef::Knife::Cloud::OpenstackFloatingIpList do
 
   context 'functionality' do
     before do
-      resources = [ TestResource.new({"instance_id" => "daed9e86-4b69-4242-993a-926a39352783", "ip" => "173.236.251.98", "fixed_ip" => "", "id" => "floatingip1", "pool" => "test-pool"}
+      resources = [ TestResource.new({ "id" => "floatingip1", "instance_id" => "daed9e86-4b69-4242-993a-926a39352783", "ip" => "173.236.251.98", "fixed_ip" => "", "pool" => "test-pool"}
 ),
-                    TestResource.new({"instance_id" => "", "ip" => "67.205.60.122", "fixed_ip" => "10.10.10.1", "id" => "floatingip2", "pool" => "test-pool"}
+                    TestResource.new({ "id" => "floatingip2", "instance_id" => "", "ip" => "67.205.60.122", "fixed_ip" => "10.10.10.1", "pool" => "test-pool" }
 )
                    ]
       allow(instance).to receive(:query_resource).and_return(resources)
       allow(instance).to receive(:puts)
       allow(instance).to receive(:create_service_instance).and_return(Chef::Knife::Cloud::Service.new)
       allow(instance).to receive(:validate!)
+      instance.config[:format] = "summary"
     end
 
     it "lists formatted list of resources" do
-      expect(instance.ui).to receive(:list).with(['Instance ID', 'IP Address', 'Fixed IP', 'Floating IP Pool',
-                                              'daed9e86-4b69-4242-993a-926a39352783', '173.236.251.98', '' 'test-pool',
-                                              '', '67.205.60.122', '10.10.10.1', 'test-pool'], :uneven_columns_across, 4)
+      expect(instance.ui).to receive(:list).with(['ID', 'Instance ID', 'IP Address', 'Fixed IP', 'Floating IP Pool',
+                                             'floatingip1', 'daed9e86-4b69-4242-993a-926a39352783', '173.236.251.98', '', 'test-pool',
+                                             'floatingip2','', '67.205.60.122', '10.10.10.1', 'test-pool'], :uneven_columns_across, 5)
       instance.run
     end
   end
