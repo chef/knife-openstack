3d2ce11747898477e8a438082ede6850fbec1ff5
dmlb2000 <dmlb2000@gmail.com>
2016-05-06 12:42:10 -0700

    Add a mechanism to use the block device mapping v2
    This puts the openstack_volumes_v2 object in the config right into
    the server config at block_device_mapping_v2 so that things like
    boot order can be utilized and such.

--- lib/chef/knife/openstack_server_create.rb
+++ lib/chef/knife/openstack_server_create.rb
@@ -64,6 +64,9 @@ class Chef
               }
             end
           end
+          unless locate_config_value(:openstack_volumes_v2).nil?
+            @create_options[:server_def][:block_device_mapping_v2] = locate_config_value(:openstack_volumes)
+          end
 
           @create_options[:server_def].merge!(user_data: locate_config_value(:user_data)) if locate_config_value(:user_data)
           @create_options[:server_def].merge!(nics: locate_config_value(:network_ids).map { |nic| nic_id = { 'net_id' => nic } }) if locate_config_value(:network_ids)
